ef65aaf2c342f242b50eb2048d89fce023b97d31
John Osterfeld <jczarfeld@gmail.com>
2021-06-23 18:08:13 -0400

    added variable arrays and the length prompt.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,14 @@
+var specChars = [ "@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".", ];
+var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerChars = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+var upperChars = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+
+var passLength= prompt("Enter character length of desired password");
+console.log(passLength);
+
+
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
